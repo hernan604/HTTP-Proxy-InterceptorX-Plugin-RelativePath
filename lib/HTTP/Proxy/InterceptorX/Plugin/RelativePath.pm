43a3b6c1db45123b09c83c71da1212530ff21895
Hernan Lopes <aut_hlopes@uolinc.com>
2013-11-26 21:36:10 -0200

    api changes

--- lib/HTTP/Proxy/InterceptorX/Plugin/RelativePath.pm
+++ lib/HTTP/Proxy/InterceptorX/Plugin/RelativePath.pm
@@ -7,19 +7,8 @@ our $VERSION = '0.01';
 use Moose::Role;
 use Path::Class;
 
-=head2
-
-This plugin maps a url path to a local directory. ie: 
-
-    remote path: http://www.site.com.br/scripts/js/(.+)
-     local path:                      /home/user/js/....
-
-Vai tentar pegar nos mesmos diretórios mas vai abrir arquivos locais ao invés de remotos
-
-=cut
-
 sub RelativePath {
-  my ( $self, $args ) = @_; 
+  my ( $self, $args ) = @_;
   foreach my $url ( keys $self->urls_to_proxy ) {
     next unless exists $self->urls_to_proxy->{ $url }->{ RelativePath }
                     && $self->http_request->{ _uri }->as_string =~ m/$url/;
@@ -36,7 +25,7 @@ sub RelativePath {
 }
 
 after 'BUILD'=>sub {
-    my ( $self ) = @_; 
+    my ( $self ) = @_;
     $self->append_plugin_method( "RelativePath" );
 };
 
@@ -47,15 +36,46 @@ __END__
 
 =head1 NAME
 
-HTTP::Proxy::InterceptorX::Plugin::RelativePath - Blah blah blah
+HTTP::Proxy::InterceptorX::Plugin::RelativePath - Maps a website path into local path
 
 =head1 SYNOPSIS
 
-  use HTTP::Proxy::InterceptorX::Plugin::RelativePath;
+    package My::Custom::Proxy;
+    use Moose;
+    extends qw/HTTP::Proxy::Interceptor/;
+    with qw/
+        HTTP::Proxy::InterceptorX::Plugin::RelativePath
+    /;
+    1;
+
+    my $p = My::Custom::Proxy->new(
+      config_path => 'config_file.pl',
+      port        => 9919,
+    );
+
+    $p->start;
+    1;
+
+=head1 CONFIG
+
+create a config file
+
+    {
+        "http://some.site.com/some/dir/path/(?<path>.+)" => {
+          RelativePath   => "/home/user/map/to/this/path/"
+        },
+        "http://some.site.com/some/dir/path/(.+)" => {
+          RelativePath   => "/home/user/map/to/this/path/"
+        },
+    }
+
+then start the proxy
 
 =head1 DESCRIPTION
 
-HTTP::Proxy::InterceptorX::Plugin::RelativePath is
+HTTP::Proxy::InterceptorX::Plugin::RelativePath allows you to map a remote path to a local path.
+
+Every file the browser try to access inside that path with be returned contents from your local path.
 
 =head1 AUTHOR
 
